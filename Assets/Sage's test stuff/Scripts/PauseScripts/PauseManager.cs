896f6734190c2bb344f3b37bc514d6f8b766679c
TheEmeraldEnd <78377336+TheEmeraldEnd@users.noreply.github.com>
2022-09-28 18:43:59 -0500

    Adds progress I've made
    Includes bolts roughly working, functioning pause (not unpause) menu,  a bit buggy character controller, a math script, bolt scrolling, and some test objects

---
+++ Assets/Sage's test stuff/Scripts/PauseScripts/PauseManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Used to organize pause menu functionality
+/// </summary>
+public class PauseManager : MonoBehaviour
+{
+    //Used to get the player input manager
+    public PlayerInput playerInput;
+
+    //The canvases used in display
+    public GameObject pauseCanvas;
+    public GameObject playerHUD;
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        //Used to freeze the game
+        Time.timeScale = 0f;
+
+        //Used to switch the player input
+        playerInput.SwitchCurrentActionMap("UI");
+
+        //Used to unlock the cursor from the screen
+        Cursor.lockState = CursorLockMode.None;
+
+        //Used to switch canvases
+        pauseCanvas.SetActive(true);
+        playerHUD.SetActive(false);
+    }
+
+    public void OnUnPause(InputAction.CallbackContext context)
+    {
+        //Used to freeze the game
+        Time.timeScale = 1f;
+
+        //Used to switch the player input
+        playerInput.SwitchCurrentActionMap("Player");
+
+        //Used to unlock the cursor from the screen
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //Used to switch canvases
+        pauseCanvas.SetActive(false);
+        playerHUD.SetActive(true);
+    }
+
+    public void FreezeTime()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeTime()
+    {
+        Time.timeScale = 1f;
+    }
+
+}
