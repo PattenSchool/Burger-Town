9cb41401469dcf68eb74a4df4f2f28f19889b9b4
TheEmeraldEnd <78377336+TheEmeraldEnd@users.noreply.github.com>
2022-10-31 12:22:56 -0500

    Save and load manager can get access to bolt types
    Can get a bolt prefab from the scriptable objects

--- Assets/Scripts/SaveSystem/SaveLoadManager.cs
+++ Assets/Scripts/SaveSystem/SaveLoadManager.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 
 public class SaveLoadManager : MonoBehaviour
 {
+    [SerializeField]
+    private LevelData_SO levelData;
+    [SerializeField]
+    private OverarchingGameData_SO gamedata;
+    private void Awake()
+    {
+        for (int i = 0; i < gamedata.boltTemplates.Count; i++)
+        {
+            //Get the enum index (because index is stored in powers of 2)
+            int enumIndex = (int)Mathf.Pow(2, i);
 
+            //Get the bolt type
+            BoltTypes lookingAtBoltType = (BoltTypes)enumIndex;
 
+            if (levelData.allowedBoltTypes.HasFlag(lookingAtBoltType))
+            {
+                //Get the bolt associated with that type
+                GameObject bolt = gamedata.boltTemplates[i - 1].gameObject;
+
+                //Print the name
+                print(bolt.name);
+            }
+        }
+    }
 
 }
